27c2f8df4245190fdfa7a3c84a36c5ef89343e26
wyc <86147974@qq.com>
2023-12-28 14:52:58 +0800

    fix: notify room update when user enter

--- src/main/java/com/wyc/bgswitch/controller/websocket/RoomWsController.java
+++ src/main/java/com/wyc/bgswitch/controller/websocket/RoomWsController.java
@@ -3,6 +3,7 @@ package com.wyc.bgswitch.controller.websocket;
 import com.wyc.bgswitch.entity.ChatMessage;
 import com.wyc.bgswitch.service.RoomService;
 import com.wyc.bgswitch.service.message.ChatMessageService;
+import com.wyc.bgswitch.service.message.RoomMessageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -21,11 +22,13 @@ public class RoomWsController {
 
     private final ChatMessageService chatService;
     private final RoomService roomService;
+    private final RoomMessageService roomMessageService;
 
     @Autowired
-    RoomWsController(ChatMessageService chatService, RoomService citadelService) {
+    RoomWsController(ChatMessageService chatService, RoomService citadelService, RoomMessageService roomMessageService) {
         this.chatService = chatService;
         this.roomService = citadelService;
+        this.roomMessageService = roomMessageService;
     }
 
 
@@ -36,6 +39,7 @@ public class RoomWsController {
         System.out.println(roomId);
         System.out.println(user);
         roomService.userEnterRoom(user, roomId);
+        roomMessageService.notifyUpdate(roomService.getRoomInfo(roomId));
         ChatMessage msg = new ChatMessage();
         msg.setContent(String.format("%s enter room %s.", user, roomId));
         msg.setIsSystemMsg(true);
